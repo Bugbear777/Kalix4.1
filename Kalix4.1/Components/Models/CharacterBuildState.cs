e3a560cb05660fa6faeecfb7925f876299d23b76
Joseph Marlow <jmarlow@lvmsystems.com>
2025-06-18 12:31:49 -0700

    cghanges made

---
+++ Kalix4.1/Components/Models/CharacterBuildState.cs
@@ -0,0 +1,24 @@
+﻿namespace Kalix4.Components.Models
+{
+  public class CharacterBuildState
+  {
+    public string Species { get; set; }
+    public string Culture { get; set; }
+    public string Nationality { get; set; }
+    public string Background { get; set; }
+
+    public Dictionary<string, int> AbilityScores { get; set; } = new()
+        {
+            { "Strength", 0 },
+            { "Dexterity", 0 },
+            { "Constitution", 0 },
+            { "Intelligence", 0 },
+            { "Wisdom", 0 },
+            { "Charisma", 0 }
+        };
+
+    public List<string> Skills { get; set; } = new();
+    public List<string> Equipment { get; set; } = new();
+  }
+}
+
---
+++ Kalix4.1/Components/Models/SpeciesCard.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalix4.Components.Models
+{
+  public class SpeciesCard
+  {
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Summary { get; set; }
+    public string PageUrl { get; set; }
+  }
+}
